19f746c44a59a419dd49b0869e28002cfec6805a
John Helder <johnhelderjp@gmail.com>
2022-12-27 20:41:43 -0300

    Criando a função iniciar som

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 //Criando evento
 document.body.addEventListener('keyup', (event)=>{
   
-})
+});
+
+//Iniciar o som
+function playSound(sound){
+  let audioElement = document.querySelector(`#s_${sound}`);
+
+  if(audioElement){
+    audioElement.play();
+  }
+}
