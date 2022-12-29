0dd017d3ec803a0ad564398b56ef8478b9bdc22f
John Helder <johnhelderjp@gmail.com>
2022-12-29 20:39:26 -0300

    Chamando a função iniciar composição

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,42 @@
 //Criando evento
 document.body.addEventListener('keyup', (event)=>{
-  
-  playSound(event.code.toLowerCase());
+  playSound( event.code.toLowerCase() );
 });
 
-document.querySelector('.composer button').addEventListener('click', ()=>{
+document.querySelector('.composer button').addEventListener('click', () => {
   let song = document.querySelector('#input').value;
 
-  if(song !== ''){
-    let songArray = song.split('');
+  if(song !== '') {
+      let songArray = song.split('');
+      playComposition(songArray);
   }
 });
 
-//Iniciar o som
-function playSound(sound){
+function playSound(sound) {
   let audioElement = document.querySelector(`#s_${sound}`);
   let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
-  if(audioElement){
-    audioElement.currentTime = 0;
-    audioElement.play();
-  }  
+  if(audioElement) {
+      audioElement.currentTime = 0;
+      audioElement.play();
+  }
 
-  
-  if(keyElement){
-    keyElement.classList.add('active');
+  if(keyElement) {
+      keyElement.classList.add('active');
 
-    setTimeout(()=>{
-      keyElement.classList.remove('active');
-    }, 300);
+      setTimeout(()=>{
+          keyElement.classList.remove('active');
+      }, 300);
   }
 }
 
-function playComposition(songArray){
+function playComposition(songArray) {
   let wait = 0;
 
-  for(let songItem of songArray){
-    setTimeout(()=>{
-      playSound(`key${songItem}`); 
-
-    }, wait);
-     wait += 250;
+  for(let songItem of songArray) {
+      setTimeout(()=>{
+          playSound(`key${songItem}`);
+      }, wait);
+      wait += 250;
   }
 }
